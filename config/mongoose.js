a1a386536381d98106296d8d5626a93945cc6215
AvirutRut <60030112@kmitl.ac.th>
2020-11-24 18:06:32 +0700

    edit

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,6 +2,6 @@ var mongoose=require('mongoose');
 
 module.exports=()=>{
     mongoose.set('debug',true);
-    var db=mongoose.connect('mongodb://localhost/s60030037');
+    var db=mongoose.connect('mongodb://localhost/s60030112');
     return db;
 }
